229e59c474730213ac4414ff1e679810ce2dfacf
tinamanga <tinamanga@gmail.com>
2025-04-18 10:59:07 +0300

    the useEffect hook to fetch data from an API Use event handlers with fetch to send data to an API Update state after receiving data from an API

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,10 @@
 
+
 module.exports = {
-  presets: ['react-app'],
-  plugins: ['@babel/plugin-proposal-private-property-in-object'],
+  presets: ['react-app'], 
+  plugins: [
+    ['@babel/plugin-transform-class-properties', { loose: true }],
+    ['@babel/plugin-transform-private-methods', { loose: true }],
+    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+  ],
 };
